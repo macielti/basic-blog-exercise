363dda607acf8d62c5dd37cbc14e357a3d80c980
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-05-31 22:48:41 -0300

    Class pages added. And initial developing of the templates.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # basic-blog-exercise
 This is a simple exercise for web development with python and Google App Engine.
+
+Link for visualization: [basic-blog-project.appspot.com](http://basic-blog-project.appspot.com/)
--- main.py
+++ main.py
@@ -5,9 +5,22 @@ import os
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = true)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Post(db.Model):
     title = db.StringProperty(required=True)
     text = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+        t = jinja_env.get_template('index.html')
+        self.response.out.write(t.render( posts= posts ))
+
+class NewPost(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template('new-post.html')
+        self.response.out.write(t.render())
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/new-post', NewPost)])
